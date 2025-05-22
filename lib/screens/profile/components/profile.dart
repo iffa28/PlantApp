3074fbceb62612ab8e783eabf99b579c3f091ff1
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-23 04:40:35 +0700

    menambahkan widget untuk halaman profile

---
+++ lib/screens/profile/components/profile.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:plantapp/constraint.dart';
+import 'package:plantapp/screens/profile/components/camera_page.dart';
+import 'package:plantapp/screens/profile/components/map_page.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  File? _imageFile;
+  final _addressController = TextEditingController();
+  String? alamatDipilih;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final Color pastel = const Color(0xFFFDEAE7); // pastel peach
+    final Color textColor = Colors.black87;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        title: const Text("Profil", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      
+    );
+  }
+
+  Widget sectionTitle(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Text(text,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+    );
+  }
+}
