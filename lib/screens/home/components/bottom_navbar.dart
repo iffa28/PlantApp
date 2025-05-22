5da9b5a20c61d453e2da770d61b661edd0c1915f
Iffatuz Zahra <iffatuzzahra28@gmail.com>
2025-05-23 03:08:03 +0700

    menambahkan navbar pada halaman home

---
+++ lib/screens/home/components/bottom_navbar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:plantapp/constraint.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: kDefaultPadding * 2,
+      right: kDefaultPadding * 2,
+      bottom: kDefaultPadding),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -10),
+            blurRadius: 35,
+            color: kPrimaryColor.withOpacity(0.38),
+          )
+        ]
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(onPressed: (){}, icon: SvgPicture.asset("assets/icons/home.svg")),
+          IconButton(onPressed: (){}, icon: SvgPicture.asset("assets/icons/love.svg")),
+          IconButton(onPressed: (){}, icon: SvgPicture.asset("assets/icons/profile.svg")), 
+        ],
+      ),
+    
+    );
+  }
+}
